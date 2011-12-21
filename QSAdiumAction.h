ffe7a739a3285df3dddd4e0a1c91564a0ec227d3
Rob McBroom <github@skurfer.com>
2011-12-21 14:09:13 -0500

    actions for sending messages and setting status

--- QSAdiumAction.h
+++ QSAdiumAction.h
@@ -5,12 +5,14 @@
 //  Created by Rob McBroom on 2011/12/9.
 //
 
-#import <QSCore/QSActionProvider.h>
+//#import <QSCore/QSActionProvider.h>
 #import "QSAdiumDefines.h"
+#import "Adium.h"
 
 #define kQSAdiumAction @"QSAdiumAction"
 
 @interface QSAdiumAction : QSActionProvider
 {
+	AdiumApplication *Adium;
 }
 @end
--- QSAdiumAction.m
+++ QSAdiumAction.m
@@ -5,11 +5,119 @@
 //  Created by Rob McBroom on 2011/12/9.
 //
 
+// TODO set status for individual account
+
 #import "QSAdiumAction.h"
 
 @implementation QSAdiumAction
 
-- (QSObject *)performActionOnObject:(QSObject *)dObject{
+- (id)init {
+    self = [super init];
+    if (self) {
+        Adium = [[SBApplication applicationWithBundleIdentifier:@"com.adiumX.adiumX"] retain];
+    }
+    return self;
+}
+
+- (void)dealloc {
+    [Adium release];
+    [super dealloc];
+}
+
+- (QSObject *)performActionOnObject:(QSObject *)dObject
+{
 	return nil;
 }
+
+- (QSObject *)sendMessage:(QSObject *)dObject to:(QSObject *)iObject
+{
+	NSString *message = [dObject stringValue];
+	AdiumContact *contact = [iObject objectForType:kQSAdiumContactType];
+	NSPredicate *chatFilter = [NSPredicate predicateWithFormat:@"name == %@", [contact name]];
+	AdiumChat *chat = [[[Adium chats] filteredArrayUsingPredicate:chatFilter] objectAtIndex:0];
+	if ([chat exists]) {
+//		[chat sendMessage:message withFile:nil];
+	} else {
+		//[Aduim sendEvent:'core' id:'crel' parameters:'Pncw', chatWindow, 0];
+//		NSLog(@"create chat window");
+		NSDictionary *windowProps = [NSDictionary dictionaryWithObjectsAndKeys:[contact displayName], @"name", nil];
+		AdiumChatWindow *chatWindow = [[[Adium classForScriptingClass:@"chat window"] alloc] initWithProperties:windowProps];
+		NSLog(@"add window to application");
+		[[Adium chatWindows] insertObject:chatWindow atIndex:0];
+//		[chatWindow moveTo:Adium];
+		NSLog(@"chat windows: %@", [[Adium chatWindows] valueForKey:@"name"]);
+		NSArray *contacts = [NSArray arrayWithObject:contact];
+		NSDictionary *props = [NSDictionary dictionaryWithObjectsAndKeys:[contact name], @"name", contacts, @"contacts", nil];
+		NSLog(@"create chat");
+		chat = [[[Adium classForScriptingClass:@"chat"] alloc] initWithProperties:props];
+//		NSLog(@"add chat to window");
+//		[[chatWindow chats] addObject:chat];
+		NSLog(@"add chat to window");
+		[[chatWindow chats] insertObject:chat atIndex:0];
+//		NSLog(@"add contact to chat");
+//		[[[[Adium chats] objectAtIndex:0] contacts] insertObject:contact atIndex:0];
+		NSLog(@"send message");
+		[chat sendMessage:message withFile:nil];
+		[chat release];
+		[chatWindow release];
+	}
+	return nil;
+}
+
+- (QSObject *)setStatus:(QSObject *)dObject to:(QSObject *)iObject
+{
+	AdiumStatus *status = [iObject objectForType:kQSAdiumStatusType];
+	[Adium setGlobalStatus:status];
+	return nil;
+}
+
+- (NSArray *)validIndirectObjectsForAction:(NSString *)action directObject:(QSObject *)dObject
+{
+	if ([action isEqualToString:@"QSAdiumSendMessageTo"]) {
+		NSMutableArray *contacts = [NSMutableArray arrayWithCapacity:1];
+		QSObject *object;
+		// list on-line contacts
+		NSPredicate *onlineFilter = [NSPredicate predicateWithFormat:@"statusType == %i", AdiumStatusTypesAvailable];
+		NSArray *onlineContacts = [[[Adium contacts] get] filteredArrayUsingPredicate:onlineFilter];
+		for (AdiumContact *contact in onlineContacts) {
+			object = [QSObject objectWithName:[contact displayName]];
+			//[child setDetails:[contact statusMessage]];
+			[object setIcon:[contact image]];
+			[object setObject:contact forType:kQSAdiumContactType];
+			[object setPrimaryType:kQSAdiumContactType];
+			[contacts addObject:object];
+		}
+		return contacts;
+	} else if ([action isEqualToString:@"QSAdiumSetStatusTo"]) {
+		NSMutableArray *statusObjects = [NSMutableArray arrayWithCapacity:1];
+		QSObject *object;
+		// list all defined statuses
+		for (AdiumStatus *status in [Adium statuses]) {
+			object = [QSObject objectWithName:[status title]];
+			[object setObject:status forType:kQSAdiumStatusType];
+			[object setPrimaryType:kQSAdiumStatusType];
+			switch ([status statusType]) {
+				case AdiumStatusTypesAvailable:
+					[object setIcon:[QSResourceManager imageNamed:@"AdiumStatusAvailable"]];
+					break;
+				case AdiumStatusTypesAway:
+					[object setIcon:[QSResourceManager imageNamed:@"AdiumStatusAway"]];
+					break;
+				case AdiumStatusTypesOffline:
+					[object setIcon:[QSResourceManager imageNamed:@"AdiumStatusOffline"]];
+					break;
+				case AdiumStatusTypesInvisible:
+					[object setIcon:[QSResourceManager imageNamed:@"AdiumStatusHidden"]];
+					break;
+				default:
+					[object setIcon:[QSResourceManager imageNamed:@"AdiumStatusAvailable"]];
+					break;
+			}
+			[statusObjects addObject:object];
+		}
+		return statusObjects;
+	}
+	return nil;
+}
+
 @end
