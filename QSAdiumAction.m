e4c175e361e775a9a1bbc5aec7c1904bd2b39b0c
Rob McBroom <github@skurfer.com>
2011-12-21 15:20:34 -0500

    rework send message since it currently only works with active chats

--- QSAdiumAction.m
+++ QSAdiumAction.m
@@ -32,34 +32,40 @@
 - (QSObject *)sendMessage:(QSObject *)dObject to:(QSObject *)iObject
 {
 	NSString *message = [dObject stringValue];
-	AdiumContact *contact = [iObject objectForType:kQSAdiumContactType];
-	NSPredicate *chatFilter = [NSPredicate predicateWithFormat:@"name == %@", [contact name]];
-	AdiumChat *chat = [[[Adium chats] filteredArrayUsingPredicate:chatFilter] objectAtIndex:0];
+//	AdiumContact *contact = [iObject objectForType:kQSAdiumContactType];
+//	NSPredicate *chatFilter = [NSPredicate predicateWithFormat:@"name == %@", [contact name]];
+//	AdiumChat *chat = [[[Adium chats] filteredArrayUsingPredicate:chatFilter] objectAtIndex:0];
+	AdiumChat *chat = [iObject objectForType:kQSAdiumChatType];
 	if ([chat exists]) {
-//		[chat sendMessage:message withFile:nil];
+		[chat sendMessage:message withFile:nil];
 	} else {
-		//[Aduim sendEvent:'core' id:'crel' parameters:'Pncw', chatWindow, 0];
+		// with apple events
+//		[Adium sendEvent:'core' id:'crel' parameters:'kocl', @"chat", 'Pwct', [NSArray arrayWithObject:contact], 'Pncw', YES, 0];
+//		[[[Adium chats] lastObject] sendMessage:message withFile:nil];
+		
+		
+		// with scripting bridge
 //		NSLog(@"create chat window");
-		NSDictionary *windowProps = [NSDictionary dictionaryWithObjectsAndKeys:[contact displayName], @"name", nil];
-		AdiumChatWindow *chatWindow = [[[Adium classForScriptingClass:@"chat window"] alloc] initWithProperties:windowProps];
-		NSLog(@"add window to application");
-		[[Adium chatWindows] insertObject:chatWindow atIndex:0];
+//		NSDictionary *windowProps = [NSDictionary dictionaryWithObjectsAndKeys:[contact displayName], @"name", nil];
+//		AdiumChatWindow *chatWindow = [[[Adium classForScriptingClass:@"chat window"] alloc] initWithProperties:windowProps];
+//		NSLog(@"add window to application");
+//		[[Adium chatWindows] insertObject:chatWindow atIndex:0];
 //		[chatWindow moveTo:Adium];
-		NSLog(@"chat windows: %@", [[Adium chatWindows] valueForKey:@"name"]);
-		NSArray *contacts = [NSArray arrayWithObject:contact];
-		NSDictionary *props = [NSDictionary dictionaryWithObjectsAndKeys:[contact name], @"name", contacts, @"contacts", nil];
-		NSLog(@"create chat");
-		chat = [[[Adium classForScriptingClass:@"chat"] alloc] initWithProperties:props];
+//		NSLog(@"chat windows: %@", [[Adium chatWindows] valueForKey:@"name"]);
+//		NSArray *contacts = [NSArray arrayWithObject:contact];
+//		NSDictionary *props = [NSDictionary dictionaryWithObjectsAndKeys:[contact name], @"name", contacts, @"contacts", nil];
+//		NSLog(@"create chat");
+//		chat = [[[Adium classForScriptingClass:@"chat"] alloc] initWithProperties:props];
 //		NSLog(@"add chat to window");
 //		[[chatWindow chats] addObject:chat];
-		NSLog(@"add chat to window");
-		[[chatWindow chats] insertObject:chat atIndex:0];
+//		NSLog(@"add chat to window");
+//		[[chatWindow chats] insertObject:chat atIndex:0];
 //		NSLog(@"add contact to chat");
 //		[[[[Adium chats] objectAtIndex:0] contacts] insertObject:contact atIndex:0];
-		NSLog(@"send message");
-		[chat sendMessage:message withFile:nil];
-		[chat release];
-		[chatWindow release];
+//		NSLog(@"send message");
+//		[chat sendMessage:message withFile:nil];
+//		[chat release];
+//		[chatWindow release];
 	}
 	return nil;
 }
@@ -74,20 +80,32 @@
 - (NSArray *)validIndirectObjectsForAction:(NSString *)action directObject:(QSObject *)dObject
 {
 	if ([action isEqualToString:@"QSAdiumSendMessageTo"]) {
-		NSMutableArray *contacts = [NSMutableArray arrayWithCapacity:1];
+//		NSMutableArray *contacts = [NSMutableArray arrayWithCapacity:1];
+//		QSObject *object;
+//		// list on-line contacts
+//		NSPredicate *onlineFilter = [NSPredicate predicateWithFormat:@"statusType == %i", AdiumStatusTypesAvailable];
+//		NSArray *onlineContacts = [[[Adium contacts] get] filteredArrayUsingPredicate:onlineFilter];
+//		for (AdiumContact *contact in onlineContacts) {
+//			object = [QSObject objectWithName:[contact displayName]];
+//			//[child setDetails:[contact statusMessage]];
+//			[object setIcon:[contact image]];
+//			[object setObject:contact forType:kQSAdiumContactType];
+//			[object setPrimaryType:kQSAdiumContactType];
+//			[contacts addObject:object];
+//		}
+//		return contacts;
+		// list active chats
+		NSMutableArray *chats = [NSMutableArray arrayWithCapacity:1];
 		QSObject *object;
-		// list on-line contacts
-		NSPredicate *onlineFilter = [NSPredicate predicateWithFormat:@"statusType == %i", AdiumStatusTypesAvailable];
-		NSArray *onlineContacts = [[[Adium contacts] get] filteredArrayUsingPredicate:onlineFilter];
-		for (AdiumContact *contact in onlineContacts) {
-			object = [QSObject objectWithName:[contact displayName]];
-			//[child setDetails:[contact statusMessage]];
-			[object setIcon:[contact image]];
-			[object setObject:contact forType:kQSAdiumContactType];
-			[object setPrimaryType:kQSAdiumContactType];
-			[contacts addObject:object];
+		for (AdiumChat *chat in [Adium chats]) {
+			NSString *chatName = [[[chat contacts] valueForKey:@"displayName"] componentsJoinedByString:@", "];
+			object = [QSObject objectWithName:chatName];
+			[object setObject:chat forType:kQSAdiumChatType];
+			[object setPrimaryType:kQSAdiumChatType];
+			[object setIcon:[[[chat contacts] objectAtIndex:0] image]];
+			[chats addObject:object];
 		}
-		return contacts;
+		return chats;
 	} else if ([action isEqualToString:@"QSAdiumSetStatusTo"]) {
 		NSMutableArray *statusObjects = [NSMutableArray arrayWithCapacity:1];
 		QSObject *object;
